05ddf7a951d92d841d80ac6960dea177e6843a37
William CADIRY <115066393+willcad@users.noreply.github.com>
2026-06-06 05:10:58 +0200

    Update restore.sh

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+set -e
+
+BACKUP_FILE=$1
+
+if [ -z "$BACKUP_FILE" ]
+then
+  echo "Usage: restore.sh backup.sql"
+  exit 1
+fi
+
+cat "$BACKUP_FILE" | docker exec -i keycloak-postgres \
+psql \
+-U keycloak \
+keycloak
+
+echo "Restore completed."
